de4353c59358f165e0e6e3f3fe997296368f0a61
aluno <aluno@INTRAUECA.CEFET.COM>
2023-02-28 11:14:52 -0300

    Form Updates

--- project-hospital/MainForm.Designer.cs
+++ project-hospital/MainForm.Designer.cs
@@ -31,10 +31,10 @@ namespace project_hospital
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.panel = new System.Windows.Forms.Panel();
+            this.btnAppointment = new System.Windows.Forms.Button();
+            this.btnRegisterClient = new System.Windows.Forms.Button();
             this.btnRegisterUser = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.btnRegisterClient = new System.Windows.Forms.Button();
-            this.btnAppointment = new System.Windows.Forms.Button();
             this.panel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,19 +46,37 @@ namespace project_hospital
             this.panel.Controls.Add(this.lblTitle);
             this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel.Location = new System.Drawing.Point(0, 0);
-            this.panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(531, 298);
+            this.panel.Size = new System.Drawing.Size(398, 242);
             this.panel.TabIndex = 0;
             this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // btnAppointment
+            // 
+            this.btnAppointment.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAppointment.Location = new System.Drawing.Point(127, 194);
+            this.btnAppointment.Name = "btnAppointment";
+            this.btnAppointment.Size = new System.Drawing.Size(144, 23);
+            this.btnAppointment.TabIndex = 3;
+            this.btnAppointment.Text = "Agendar Serviço";
+            this.btnAppointment.UseVisualStyleBackColor = true;
+            // 
+            // btnRegisterClient
+            // 
+            this.btnRegisterClient.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegisterClient.Location = new System.Drawing.Point(32, 141);
+            this.btnRegisterClient.Name = "btnRegisterClient";
+            this.btnRegisterClient.Size = new System.Drawing.Size(144, 23);
+            this.btnRegisterClient.TabIndex = 2;
+            this.btnRegisterClient.Text = "Cadastrar Cliente";
+            this.btnRegisterClient.UseVisualStyleBackColor = true;
             // 
             // btnRegisterUser
             // 
             this.btnRegisterUser.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegisterUser.Location = new System.Drawing.Point(301, 173);
-            this.btnRegisterUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRegisterUser.Location = new System.Drawing.Point(226, 141);
             this.btnRegisterUser.Name = "btnRegisterUser";
-            this.btnRegisterUser.Size = new System.Drawing.Size(192, 28);
+            this.btnRegisterUser.Size = new System.Drawing.Size(144, 23);
             this.btnRegisterUser.TabIndex = 1;
             this.btnRegisterUser.Text = "Cadastrar Usuário";
             this.btnRegisterUser.UseVisualStyleBackColor = true;
@@ -69,44 +87,20 @@ namespace project_hospital
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Verdana", 14.2F);
-            this.lblTitle.Location = new System.Drawing.Point(37, 78);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Location = new System.Drawing.Point(16, 60);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(456, 29);
+            this.lblTitle.Size = new System.Drawing.Size(370, 23);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Bem vindo ao Hospital Santo Arnaldo";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // btnRegisterClient
-            // 
-            this.btnRegisterClient.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegisterClient.Location = new System.Drawing.Point(42, 173);
-            this.btnRegisterClient.Margin = new System.Windows.Forms.Padding(4);
-            this.btnRegisterClient.Name = "btnRegisterClient";
-            this.btnRegisterClient.Size = new System.Drawing.Size(192, 28);
-            this.btnRegisterClient.TabIndex = 2;
-            this.btnRegisterClient.Text = "Cadastrar Cliente";
-            this.btnRegisterClient.UseVisualStyleBackColor = true;
-            // 
-            // btnAppointment
-            // 
-            this.btnAppointment.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAppointment.Location = new System.Drawing.Point(169, 239);
-            this.btnAppointment.Margin = new System.Windows.Forms.Padding(4);
-            this.btnAppointment.Name = "btnAppointment";
-            this.btnAppointment.Size = new System.Drawing.Size(192, 28);
-            this.btnAppointment.TabIndex = 3;
-            this.btnAppointment.Text = "Agendar Serviço";
-            this.btnAppointment.UseVisualStyleBackColor = true;
-            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 298);
+            this.ClientSize = new System.Drawing.Size(398, 242);
             this.Controls.Add(this.panel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "Início";
             this.panel.ResumeLayout(false);
